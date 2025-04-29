3c046877c34ffb341f6507dc8bec5aecf57f97a2
adiya-deokar <adityadeokar80@gmail.com>
2025-04-29 23:01:11 +0530

    Assessment Logic added

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['img.clerk.com'],
+
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        pathname: '/**', 
+      },
+    ],
+
   },
 };
 
